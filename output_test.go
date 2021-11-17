18b0e7d7132bb480a4b0c2f06e452d0b22f1a3fa
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2021-11-17 23:34:35 +0200

    update output tests

--- output_test.go
+++ output_test.go
@@ -1,48 +1,58 @@
-//
-//   Copyright 2019 Dmitry Kolesnikov, All Rights Reserved
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
+/*
+
+  Copyright 2019 Dmitry Kolesnikov, All Rights Reserved
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
 
 package gouldian_test
 
-/*
-func TestSuccess(t *testing.T) {
-	uri, _ := url.Parse("/")
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
 
-	output(t, µ.Success(200), µ.Success(200))
-	output(t, µ.Ok(), µ.Success(200))
-	output(t, µ.Created(), µ.Success(201))
-	output(t, µ.Accepted(), µ.Success(202))
-	output(t, µ.NoContent(), µ.Success(204))
-	output(t, µ.MovedPermanently(*uri), µ.Success(301).With("Location", "/"))
-	output(t, µ.Found(*uri), µ.Success(302).With("Location", "/"))
-	output(t, µ.SeeOther(*uri), µ.Success(303).With("Location", "/"))
-	output(t, µ.NotModified(*uri), µ.Success(304).With("Location", "/"))
-	output(t, µ.TemporaryRedirect(*uri), µ.Success(307).With("Location", "/"))
-	output(t, µ.PermanentRedirect(*uri), µ.Success(308).With("Location", "/"))
+	µ "github.com/fogfish/gouldian"
+	"github.com/fogfish/gouldian/mock"
+	"github.com/fogfish/it"
+)
+
+func TestSuccess(t *testing.T) {
+	output(t, µ.NewSuccess(200), µ.NewSuccess(200))
+	output(t, µ.Status.OK(), µ.NewSuccess(200))
+	output(t, µ.Status.Created(), µ.NewSuccess(201))
+	output(t, µ.Status.Accepted(), µ.NewSuccess(202))
+	output(t, µ.Status.NoContent(), µ.NewSuccess(204))
+	output(t, µ.Status.MovedPermanently("/"), µ.NewSuccess(301).With("Location", "/"))
+	output(t, µ.Status.Found("/"), µ.NewSuccess(302).With("Location", "/"))
+	output(t, µ.Status.SeeOther("/"), µ.NewSuccess(303).With("Location", "/"))
+	output(t, µ.Status.NotModified("/"), µ.NewSuccess(304).With("Location", "/"))
+	output(t, µ.Status.TemporaryRedirect("/"), µ.NewSuccess(307).With("Location", "/"))
+	output(t, µ.Status.PermanentRedirect("/"), µ.NewSuccess(308).With("Location", "/"))
 }
 
 func TestIssue(t *testing.T) {
 	err := fmt.Errorf("issue")
-	issue(t, µ.Failure(500, ""), µ.Failure(500, ""))
-	issue(t, µ.BadRequest(err), µ.Failure(400, "").Reason(err))
-	issue(t, µ.Unauthorized(err), µ.Failure(401, "").Reason(err))
-	issue(t, µ.Forbidden(err), µ.Failure(403, "").Reason(err))
-	issue(t, µ.NotFound(err), µ.Failure(404, "").Reason(err))
-	issue(t, µ.InternalServerError(err), µ.Failure(500, "").Reason(err))
-	issue(t, µ.NotImplemented(err), µ.Failure(501, "").Reason(err))
-	issue(t, µ.ServiceUnavailable(err), µ.Failure(503, "").Reason(err))
+	issue(t, µ.NewFailure(500, err), µ.NewFailure(500, err))
+	issue(t, µ.Status.BadRequest(err), µ.NewFailure(400, err))
+	issue(t, µ.Status.Unauthorized(err), µ.NewFailure(401, err))
+	issue(t, µ.Status.Forbidden(err), µ.NewFailure(403, err))
+	issue(t, µ.Status.NotFound(err), µ.NewFailure(404, err))
+	issue(t, µ.Status.InternalServerError(err), µ.NewFailure(500, err))
+	issue(t, µ.Status.NotImplemented(err), µ.NewFailure(501, err))
+	issue(t, µ.Status.ServiceUnavailable(err), µ.NewFailure(503, err))
 }
 
 type myT struct {
@@ -51,42 +61,42 @@ type myT struct {
 
 func TestJSON(t *testing.T) {
 	output(t,
-		µ.Ok().JSON(myT{"Hello"}),
-		µ.Ok().JSON(myT{"Hello"}),
+		µ.Status.OK().JSON(myT{"Hello"}),
+		µ.Status.OK().JSON(myT{"Hello"}),
 	)
 }
 
 func TestOutputText(t *testing.T) {
 	output(t,
-		µ.Ok().Text("Hello"),
-		µ.Ok().Text("Hello"),
+		µ.Status.OK().Text("Hello"),
+		µ.Status.OK().Text("Hello"),
 	)
 }
 
 func TestOutputBytes(t *testing.T) {
 	output(t,
-		µ.Ok().Bytes([]byte("Hello")),
-		µ.Ok().Bytes([]byte("Hello")),
+		µ.Status.OK().Bytes([]byte("Hello")),
+		µ.Status.OK().Bytes([]byte("Hello")),
 	)
 }
 
-func TestErrorOnJSON(t *testing.T) {
-	output := µ.Ok().JSON(make(chan int))
+// func TestErrorOnJSON(t *testing.T) {
+// 	output := µ.Status.OK().JSON(make(chan int))
 
-	it.Ok(t).
-		If(output.Status).Should().Equal(http.StatusInternalServerError)
-}
+// 	it.Ok(t).
+// 		If(output.Status).Should().Equal(http.StatusInternalServerError)
+// }
 
-func output(t *testing.T, a, b *µ.Output) {
+func output(t *testing.T, a, b µ.Output) {
 	t.Helper()
-	foo := µ.GET(µ.FMap(func() error { return a }))
+	foo := µ.GET(func(*µ.Input) error { return a })
 	req := mock.Input()
 
 	it.Ok(t).
 		If(foo(req)).Should().
 		Assert(
 			func(be interface{}) bool {
-				var out *µ.Output
+				var out µ.Output
 				if errors.As(be.(error), &out) {
 					return reflect.DeepEqual(b, out)
 				}
@@ -95,9 +105,9 @@ func output(t *testing.T, a, b *µ.Output) {
 		)
 }
 
-func issue(t *testing.T, a, b *µ.Issue) {
+func issue(t *testing.T, a, b µ.Output) {
 	t.Helper()
-	foo := µ.GET(µ.FMap(func() error { return a }))
+	foo := µ.GET(func(*µ.Input) error { return a })
 	req := mock.Input()
 
 	it.Ok(t).
@@ -113,4 +123,3 @@ func issue(t *testing.T, a, b *µ.Issue) {
 			},
 		)
 }
-*/
