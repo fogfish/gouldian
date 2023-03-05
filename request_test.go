cf958909ed76b14ac413750995e46ab0cb0ecce8
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-03-05 12:55:20 +0200

    test coverage for request unit

--- request_test.go
+++ request_test.go
@@ -1,11 +1,29 @@
 package gouldian_test
 
 import (
+	"encoding/json"
+	"errors"
 	µ "github.com/fogfish/gouldian"
 	"github.com/fogfish/gouldian/mock"
 	"github.com/fogfish/it/v2"
+	"net/http"
 	"testing"
 )
+
+func TestHTTP(t *testing.T) {
+	foo := mock.Endpoint(
+		µ.HTTP(
+			http.MethodGet,
+			µ.URI(µ.Path("foo")),
+		),
+	)
+
+	req := mock.Input(mock.URL("/foo"))
+	err := foo(req)
+	it.Then(t).Should(
+		it.Nil(err),
+	)
+}
 
 func TestMethod(t *testing.T) {
 	spec := []struct {
@@ -238,6 +256,259 @@ func TestBodyText(t *testing.T) {
 			it.Nil(err),
 			it.Nil(µ.FromContext(tt.Mock, &req)),
 			it.Equiv(req.FooBar, tt.Expect),
+		)
+	}
+}
+
+func TestFMapSuccess(t *testing.T) {
+	type T struct{ A string }
+	a := µ.Optics1[T, string]()
+
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo"), µ.Path(a)),
+			µ.FMap(func(ctx *µ.Context, t *T) error {
+				out := µ.NewOutput(http.StatusOK)
+				out.Body = t.A
+				return out
+			}),
+		),
+	)
+	req := mock.Input(mock.URL("/foo/bar"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			return err.Error() == "bar"
+		}),
+	)
+}
+
+func TestMapSuccess(t *testing.T) {
+	type T struct{ A string }
+	a := µ.Optics1[T, string]()
+
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo"), µ.Path(a)),
+			µ.Map(func(ctx *µ.Context, t *T) (*T, error) {
+				return t, nil
+			}),
+		),
+	)
+	req := mock.Input(mock.URL("/foo/bar"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			return err.Error() == "{\"A\":\"bar\"}"
+		}),
+	)
+}
+
+func TestContextFree(t *testing.T) {
+	foo := mock.Endpoint(µ.GET(µ.URI(µ.Path("foo"))))
+	req := mock.Input(mock.URL("/foo"))
+	err := foo(req)
+
+	it.Then(t).Should(it.Nil(err))
+
+	req.Free()
+	err = foo(req)
+
+	it.Then(t).ShouldNot(it.Nil(err))
+}
+
+func TestHandlerSuccess(t *testing.T) {
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo")),
+			func(*µ.Context) error {
+				out := µ.NewOutput(http.StatusOK)
+				out.Body = "bar"
+				return out
+			},
+		),
+	)
+	req := mock.Input(mock.URL("/foo"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			return err.Error() == "bar"
+		}),
+	)
+}
+
+func TestHandler2Success(t *testing.T) {
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo")),
+			func(*µ.Context) error {
+				out := µ.NewOutput(http.StatusOK)
+				out.Body = "bar"
+				return out
+			},
+		),
+	)
+	bar := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("bar")),
+			func(*µ.Context) error {
+				out := µ.NewOutput(http.StatusOK)
+				out.Body = "foo"
+				return out
+			},
+		),
+	)
+	req := mock.Input(mock.URL("/foo"))
+	err := µ.Endpoints{foo, bar}.Or(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			return err.Error() == "bar"
+		}),
+	)
+}
+
+func TestHandlerFailure(t *testing.T) {
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo")),
+			func(*µ.Context) error {
+				out := µ.NewOutput(http.StatusUnauthorized)
+				out.SetIssue(errors.New(""))
+				return out
+			},
+		),
+	)
+	req := mock.Input(mock.URL("/foo"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			switch v := err.(type) {
+			case *µ.Output:
+				return v.Status == http.StatusUnauthorized
+			default:
+				return false
+			}
+		}),
+	)
+}
+
+func TestFMapFailure(t *testing.T) {
+	type T struct{ A string }
+	a := µ.Optics1[T, string]()
+
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo"), µ.Path(a)),
+			µ.FMap(func(*µ.Context, *T) error {
+				out := µ.NewOutput(http.StatusUnauthorized)
+				out.SetIssue(errors.New(""))
+				return out
+			}),
+		),
+	)
+	req := mock.Input(mock.URL("/foo/bar"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			switch v := err.(type) {
+			case *µ.Output:
+				return v.Status == http.StatusUnauthorized
+			default:
+				return false
+			}
+		}),
+	)
+}
+
+func TestMapFailure(t *testing.T) {
+	type T struct{ A string }
+	a := µ.Optics1[T, string]()
+
+	foo := mock.Endpoint(
+		µ.GET(
+			µ.URI(µ.Path("foo"), µ.Path(a)),
+			µ.Map(func(*µ.Context, *T) (*T, error) {
+				out := µ.NewOutput(http.StatusUnauthorized)
+				out.SetIssue(errors.New(""))
+				return nil, out
+			}),
+		),
+	)
+	req := mock.Input(mock.URL("/foo/bar"))
+	err := foo(req)
+
+	it.Then(t).Should(
+		it.Be(func() bool {
+			switch v := err.(type) {
+			case *µ.Output:
+				return v.Status == http.StatusUnauthorized
+			default:
+				return false
+			}
+		}),
+	)
+}
+
+func TestBodyLeak(t *testing.T) {
+	type Pair struct {
+		Key int    `json:"key,omitempty"`
+		Val string `json:"val,omitempty"`
+	}
+	type Item struct {
+		Seq []Pair `json:"seq,omitempty"`
+	}
+	type request struct {
+		Item Item
+	}
+	lens := µ.Optics1[request, Item]()
+
+	endpoint := func() µ.Routable {
+		return µ.GET(
+			µ.URI(),
+			µ.Body(lens),
+			func(ctx *µ.Context) error {
+				var req request
+				if err := µ.FromContext(ctx, &req); err != nil {
+					return err
+				}
+
+				seq := []Pair{}
+				for key, val := range req.Item.Seq {
+					if val.Key == 0 {
+						seq = append(seq, Pair{Key: key + 1, Val: val.Val})
+					}
+				}
+				req.Item = Item{Seq: seq}
+				out := µ.NewOutput(http.StatusOK)
+
+				val, _ := json.Marshal(req.Item)
+				out.Body = string(val)
+				return out
+			},
+		)
+	}
+
+	foo := mock.Endpoint(endpoint())
+	for val, expect := range map[string]string{
+		"{\"seq\":[{\"val\":\"a\"},{\"val\":\"b\"}]}":                 "{\"seq\":[{\"key\":1,\"val\":\"a\"},{\"key\":2,\"val\":\"b\"}]}",
+		"{\"seq\":[{\"val\":\"c\"}]}":                                 "{\"seq\":[{\"key\":1,\"val\":\"c\"}]}",
+		"{\"seq\":[{\"val\":\"d\"},{\"val\":\"e\"},{\"val\":\"f\"}]}": "{\"seq\":[{\"key\":1,\"val\":\"d\"},{\"key\":2,\"val\":\"e\"},{\"key\":3,\"val\":\"f\"}]}",
+	} {
+		req := mock.Input(
+			mock.Method("GET"),
+			mock.Header("Content-Type", "application/json"),
+			mock.Text(val),
+		)
+		out := foo(req)
+
+		it.Then(t).Should(
+			it.Equal(out.Error(), expect),
 		)
 	}
 }
