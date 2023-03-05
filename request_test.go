85ffb5cffea0d81bc0e3bbe17c1af743c13e0b28
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-03-05 12:31:36 +0200

    test coverage for body

--- request_test.go
+++ request_test.go
@@ -35,3 +35,209 @@ func TestMethod(t *testing.T) {
 		)
 	}
 }
+
+func TestMethodNoMatch(t *testing.T) {
+	spec := []struct {
+		Verb func(µ.Routable, ...µ.Endpoint) µ.Routable
+		Mock mock.Mock
+	}{
+		{µ.GET, mock.Method("OTHER")},
+		{µ.PUT, mock.Method("OTHER")},
+		{µ.POST, mock.Method("OTHER")},
+		{µ.DELETE, mock.Method("OTHER")},
+		{µ.PATCH, mock.Method("OTHER")},
+	}
+
+	for _, tt := range spec {
+		foo := mock.Endpoint(
+			tt.Verb(
+				µ.URI(µ.Path("foo")),
+			),
+		)
+
+		req := mock.Input(tt.Mock, mock.URL("/foo"))
+		err := foo(req)
+		it.Then(t).ShouldNot(
+			it.Nil(err),
+		)
+	}
+}
+
+func TestBodyJson(t *testing.T) {
+	type foobar struct {
+		Foo string `json:"foo"`
+		Bar int    `json:"bar"`
+	}
+
+	spec := []struct {
+		Mock   *µ.Context
+		Expect foobar
+	}{
+		{
+			mock.Input(
+				mock.Header("Content-Type", "application/json"),
+				mock.JSON(foobar{"foo1", 10}),
+			),
+			foobar{"foo1", 10},
+		},
+		{
+			mock.Input(
+				mock.Header("Content-Type", "application/json"),
+				mock.Text(`{"foo":"foo1","bar":10}`),
+			),
+			foobar{"foo1", 10},
+		},
+	}
+
+	type request struct {
+		FooBar foobar `content:"json"`
+	}
+	var lens = µ.Optics1[request, foobar]()
+
+	for _, tt := range spec {
+		var req request
+		foo := mock.Endpoint(µ.GET(µ.URI(), µ.Body(lens)))
+		err := foo(tt.Mock)
+
+		it.Then(t).Should(
+			it.Nil(err),
+			it.Nil(µ.FromContext(tt.Mock, &req)),
+			it.Equiv(req.FooBar, tt.Expect),
+		)
+	}
+}
+
+func TestBodyJsonNoMatch(t *testing.T) {
+	type foobar struct {
+		Foo string `json:"foo"`
+		Bar int    `json:"bar"`
+	}
+
+	spec := []struct {
+		Mock *µ.Context
+	}{
+		{
+			mock.Input(
+				mock.Header("Content-Type", "application/json"),
+				mock.Text(`{"foo:"foo1,"bar":10}`),
+			),
+		},
+	}
+
+	type request struct {
+		FooBar foobar `content:"json"`
+	}
+	var lens = µ.Optics1[request, foobar]()
+
+	for _, tt := range spec {
+		var req request
+		foo := mock.Endpoint(µ.GET(µ.URI(), µ.Body(lens)))
+		err := foo(tt.Mock)
+
+		it.Then(t).
+			Should(it.Nil(err)).
+			ShouldNot(it.Nil(µ.FromContext(tt.Mock, &req)))
+	}
+}
+
+func TestBodyForm(t *testing.T) {
+	type foobar struct {
+		Foo string `json:"foo"`
+		Bar int    `json:"bar"`
+	}
+
+	spec := []struct {
+		Mock   *µ.Context
+		Expect foobar
+	}{
+		{
+			mock.Input(
+				mock.Header("Content-Type", "application/x-www-form-urlencoded"),
+				mock.Text("foo=foo1&bar=10"),
+			),
+			foobar{"foo1", 10},
+		},
+	}
+
+	type request struct {
+		FooBar foobar `content:"form"`
+	}
+	var lens = µ.Optics1[request, foobar]()
+
+	for _, tt := range spec {
+		var req request
+		foo := mock.Endpoint(µ.GET(µ.URI(), µ.Body(lens)))
+		err := foo(tt.Mock)
+
+		it.Then(t).Should(
+			it.Nil(err),
+			it.Nil(µ.FromContext(tt.Mock, &req)),
+			it.Equiv(req.FooBar, tt.Expect),
+		)
+	}
+}
+
+func TestBodyFormNoMatch(t *testing.T) {
+	type foobar struct {
+		Foo string `json:"foo"`
+		Bar int    `json:"bar"`
+	}
+
+	spec := []struct {
+		Mock *µ.Context
+	}{
+		{
+			mock.Input(
+				mock.Header("Content-Type", "application/x-www-form-urlencoded"),
+				mock.Text("foobar"),
+			),
+		},
+	}
+
+	type request struct {
+		FooBar foobar `content:"form"`
+	}
+	var lens = µ.Optics1[request, foobar]()
+
+	for _, tt := range spec {
+		var req request
+		foo := mock.Endpoint(µ.GET(µ.URI(), µ.Body(lens)))
+		err := foo(tt.Mock)
+
+		it.Then(t).
+			Should(it.Nil(err)).
+			ShouldNot(it.Nil(µ.FromContext(tt.Mock, &req)))
+	}
+}
+
+func TestBodyText(t *testing.T) {
+	spec := []struct {
+		Mock   *µ.Context
+		Expect string
+	}{
+		{
+			mock.Input(
+				mock.Header("Content-Type", "text/plain"),
+				mock.Text("foobar"),
+			),
+			"foobar",
+		},
+	}
+
+	type request struct {
+		FooBar string
+	}
+	var lens = µ.Optics1[request, string]()
+
+	for _, tt := range spec {
+		var req request
+		foo := mock.Endpoint(µ.GET(µ.URI(), µ.Body(lens)))
+		err := foo(tt.Mock)
+
+		it.Then(t).Should(
+			it.Nil(err),
+			it.Nil(µ.FromContext(tt.Mock, &req)),
+			it.Equiv(req.FooBar, tt.Expect),
+		)
+	}
+}
